c9c0c44cd07ac1f9893700f5027c16c831daa6e7
Bert Van de Poel <bert@bhack.net>
2019-01-11 03:36:09 +0100

    New database structure for new analysis parameters, result format and interface

--- web/db.sql
+++ web/db.sql
@@ -1,14 +1,14 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `accounts` (
+CREATE TABLE `accounts` (
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `hash` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE IF NOT EXISTS `corpora` (
+CREATE TABLE `corpora` (
   `id` int(11) NOT NULL,
   `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `format` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -18,14 +18,12 @@ CREATE TABLE IF NOT EXISTS `corpora` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE IF NOT EXISTS `reports` (
+CREATE TABLE `reports` (
   `id` int(11) NOT NULL,
   `owner` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `c1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `c2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `freqnum` int(11) NOT NULL,
-  `am` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `resultnum` int(11) NOT NULL,
   `datetime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
