4ee07ffa0ca89db1e5e42bcf49cc95d1fd4c8dff
Bert Van de Poel <bert@bhack.net>
2019-06-02 05:27:55 +0200

    Mark repulsed keywords with a red background in the SLMA marker table, fixes the last element of #27

--- web/report.php
+++ web/report.php
@@ -122,10 +122,20 @@ else {
 				$i++;
 				$fields = explode("\t", $row);
 				if(isset($_GET['detailed'])) {
-					echo '<tr><td>' . $i . '</td><td class="breakwords">' . $fields[0] . '</td><td>' . $fields[1] . '</td><td>' . round($fields[2],4) . '</td><td>' . $fields[3] . '</td><td>' . $fields[4] . '</td><td>' . round($fields[5],3) . '</td><td>' . round($fields[6],3) . '</td><td>' . round($fields[7],3) . '</td><td>' . round($fields[8],3) . '</td></tr>';
+					if($fields[1] < 0) {
+						echo '<tr class="repulsion"><td>' . $i . '</td><td class="breakwords">' . $fields[0] . '</td><td>' . $fields[1] . '</td><td>' . round($fields[2],4) . '</td><td>' . $fields[3] . '</td><td>' . $fields[4] . '</td><td>' . round($fields[5],3) . '</td><td>' . round($fields[6],3) . '</td><td>' . round($fields[7],3) . '</td><td>' . round($fields[8],3) . '</td></tr>';
+					}
+					else {
+						echo '<tr><td>' . $i . '</td><td class="breakwords">' . $fields[0] . '</td><td>' . $fields[1] . '</td><td>' . round($fields[2],4) . '</td><td>' . $fields[3] . '</td><td>' . $fields[4] . '</td><td>' . round($fields[5],3) . '</td><td>' . round($fields[6],3) . '</td><td>' . round($fields[7],3) . '</td><td>' . round($fields[8],3) . '</td></tr>';
+					}
 				}
 				else {
-					echo '<tr><td>' . $i . '</td><td class="breakwords">' . $fields[0] . '</td><td>' . $fields[1] . '</td><td>' . round($fields[2],4) . '</td><td>' . round($fields[8],3) . '</td></tr>';
+					if($fields[1] < 0) {
+						echo '<tr class="repulsion"><td>' . $i . '</td><td class="breakwords">' . $fields[0] . '</td><td>' . $fields[1] . '</td><td>' . round($fields[2],4) . '</td><td>' . round($fields[8],3) . '</td></tr>';
+					}
+					else {
+						echo '<tr><td>' . $i . '</td><td class="breakwords">' . $fields[0] . '</td><td>' . $fields[1] . '</td><td>' . round($fields[2],4) . '</td><td>' . round($fields[8],3) . '</td></tr>';
+					}
 				}
 				if( !isset($_GET['allitems']) AND !isset($_GET['detailed']) AND $i == 100 ) {
 					break;
